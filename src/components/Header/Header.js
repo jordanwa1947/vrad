74922925594495567636db54996cf9542aba28eb
Lane Hunter <lanehunter.96@gmail.com>
2020-03-24 16:49:37 -0600

    Add header files and intial styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => {
+  return (
+    <header>
+      <h1>vrad. <span id="headline">— vacation rentals around denver.</span></h1>
+      <nav>
+        <button>Log Out</button>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
