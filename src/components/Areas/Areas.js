2dee54b17d4e9ec086ec90db02806a63be53dd41
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-24 15:55:33 -0600

    Adds Areas component and imports it into the App component

---
+++ src/components/Areas/Areas.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Areas extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      areas: []
+    }
+  }
+
+  render() {
+    return(
+      <h1>Hi there</h1>
+    )
+  }
+}
+
+export default Areas;
