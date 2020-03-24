70a4dca9ac31b79f69d3c49497f2daed0c94ab4f
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-24 16:42:48 -0600

    Uses nested fetching to get all of the areas and their associated info and renders it to the page

--- src/components/Areas/Areas.js
+++ src/components/Areas/Areas.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Area from '../Area/Area';
 
 class Areas extends React.Component {
   constructor() {
@@ -8,9 +9,41 @@ class Areas extends React.Component {
     }
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/areas')
+      .then(response => response.json())
+      .then(areaData => {
+        const promises = areaData.areas.map(area => {
+          return fetch(`http://localhost:3001${area.details}`)
+            .then(response => response.json())
+            .then(areaDetails => {
+              return {
+                shortname: area.area,
+                name: areaDetails.name,
+                description: areaDetails.about
+              }
+            })
+        })
+        return Promise.all(promises)
+      })
+      .then(areaData => {
+        this.setState({
+          areas: areaData
+        })
+      })
+  }
+
+  buildAreaComponents = () => {
+    return this.state.areas.map(area => {
+      return <Area key={area.id} area={area} />
+    })
+  }
+
   render() {
     return(
-      <h1>Hi there</h1>
+      <section>
+        {this.buildAreaComponents()}
+      </section>
     )
   }
 }
