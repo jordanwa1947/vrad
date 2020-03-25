18caf2cbfc699231c03977a103a692b45efa4527
Lane Hunter <lanehunter.96@gmail.com>
2020-03-25 15:36:03 -0600

    Add login form jsx and state changing methods

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,13 +7,22 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      username: ''
+    };
   }
+
+  login = (username) => {
+    this.setState({username: username})
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        
+        <main>
+          <Login login={this.login}/>
+        </main>
       </div>
     )
   }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
 
-const Login = (props) => {
-  return (
-    <h1>Login</h1>
-  )
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value})
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.props.login(this.state.username)
+  }
+
+  render() {
+    return (
+      <form>
+        <h3>Please log in</h3>
+        <input
+          type='text'
+          placeholder='username'
+          value={this.state.username}
+          name='username'
+          onChange={this.handleChange}/>
+        <input
+          type='password'
+          placeholder='password'
+          value={this.state.password}
+          name='password'
+          onChange={this.handleChange}/>
+        <button
+          onClick={this.handleClick}>
+          Log in
+        </button>
+      </form>
+    )
+  }
 }
 
 export default Login
