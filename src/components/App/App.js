def0feb1468157d09227b9af5aed5db06e82dd19
Lane Hunter <lanehunter.96@gmail.com>
2020-03-26 15:52:37 -0600

    Add listings component to src

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Areas from '../Areas/Areas';
+import Listings from '../Listings/Listings';
 import './App.css';
 
 import { Route, NavLink, Redirect } from 'react-router-dom'
@@ -26,9 +27,10 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <main>
-          <Route exact path='/areas' component={Areas} />
           <Route exact path='/' render={() => <Login login={this.login}/>} />
           {this.state.fireRedirect && <Redirect to='/areas'/>}
+          <Route exact path='/areas' component={Areas} />
+          <Route path='/areas/:area-id/listings' component={Listings} />
         </main>
       </div>
     )
