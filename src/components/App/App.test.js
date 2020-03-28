2884792f93cb21781de8b3dc80ed952dd0c0fb74
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-28 12:36:20 -0600

    Adds integration test for the App component that logs in and checks for basic test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-});
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('Render login page', () => {
+    const { getByText, getByPlaceholderText } = render(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    );
+
+    fireEvent.change(getByPlaceholderText(/name/i), { target: { value: 'name' } })
+    fireEvent.change(getByPlaceholderText(/email/i), { target: { value: 'email' } })
+    fireEvent.click(getByText("Log in"));
+
+    expect(getByText("— vacation rentals around denver.")).toBeInTheDocument();
+  });
+})
