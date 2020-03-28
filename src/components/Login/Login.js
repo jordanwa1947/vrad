28dd05f86c58b8df1897518d326cfc0a2ea42033
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-28 12:40:43 -0600

    Adds login test that fills out form and sumbits and test that a mocked login function is called

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './Login.css';
 
 class Login extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       name: '',
       email: '',
