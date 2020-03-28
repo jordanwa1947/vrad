c2fec116e9db543f74dec80a834a717952dcde6c
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-28 12:39:22 -0600

    Basic unit test for Headercomponent text

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react'; 
+import Header from './Header.js';
+ import '@testing-library/jest-dom'
+
+describe('Header', () => {
+  it('It should render the text that we expect', () => {
+    const { getByText } = render(<Header />);
+
+    expect(getByText("— vacation rentals around denver.")).toBeInTheDocument();
+  })
+})
