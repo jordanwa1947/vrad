6e2d69729a0996ac161ccca9ee3a5fcaab7eddfd
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-28 15:31:23 -0600

    Adds custom messages to the user profile

--- src/components/UserProfile/UserProfile.test.js
+++ src/components/UserProfile/UserProfile.test.js
@@ -4,11 +4,27 @@ import UserProfile from './UserProfile.js';
  import '@testing-library/jest-dom'
 
 describe('UserProfile', () => {
-  it('It should render the text that we expect', () => {
+  it('It should render the leisure message', () => {
     const { getByText } = render(
       <UserProfile travelReason="leisure"/>
     );
 
-    expect(getByText("leisure")).toBeInTheDocument();
+    expect(getByText("Ready to have some fun!?")).toBeInTheDocument();
+  })
+
+  it('It should render the business message', () => {
+    const { getByText } = render(
+      <UserProfile travelReason="business"/>
+    );
+
+    expect(getByText("Find the cheapest, quality rentals in Denver")).toBeInTheDocument();
+  })
+
+  it('It should render the other message', () => {
+    const { getByText } = render(
+      <UserProfile travelReason="other"/>
+    );
+
+    expect(getByText("Have a great trip!")).toBeInTheDocument();
   })
 })
